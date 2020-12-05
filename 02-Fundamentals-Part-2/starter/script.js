385a8e9085c18018a2dea7d8b45b714bc96a2a84
haoranyu <yu.haora@northeastern.edu>
2020-12-05 02:01:43 -0500

    arrow-functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -37,3 +37,29 @@ const calcAge2 = function (birthYear) {
 
 const age2 = calcAge2(1991);
 console.log(age2)
+
+// Arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(2011);
+console.log(age3);
+
+const yearsUntilRetirement = birthYear => {
+  const age = 2037 -birthYear;
+  const retirement = 65 - age;
+  return retirement;
+}
+
+console.log(yearsUntilRetirement(1991));
+
+const yearsUntilRetirementAndName = (birthYear, firstName) => {
+  const age = 2037 -birthYear;
+  const retirement = 65 - age;
+  return `${firstName} retires in ${retirement} years.`;
+}
+
+console.log(yearsUntilRetirementAndName(1991, 'Jonas'));
+console.log(yearsUntilRetirementAndName(1980, 'Tom'));
+
+// when to use arrow functions(function expression) and function declarations
+// fact: arrow functions do not get a so-called 'this' keyword
+
