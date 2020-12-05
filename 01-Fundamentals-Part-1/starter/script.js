7f3b24cb7a41ef1fa904a77870490c0d719158cb
haoranyu <yu.haora@northeastern.edu>
2020-12-04 20:07:41 -0500

    fundamentals-part-1-reviewed

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,41 @@
+/*
 let js = 'amazing';
 if (js === 'amazing') {alert('JavaScript is Fun!')}
 
 40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Matilda';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable naming conventions
+let jonas_matilda = 'JM';
+let $function = 27; // don't use reserved keyword for naming
+
+let person = 'jonas';
+let PI = 3.1415;
+
+let myFirstEducation = 'Wuhan University';
+let mySecondEducation = 'Northeastern University';
+
+let edu1 = 'WHU';
+let edu2 = 'NEU';
+
+console.log(myFirstEducation);
+
+*/
+
+ 
+
+/**
+ * 
+ * let is block-scoped
+ * var is function-scoped basically should never use var
+ * 
+ */
