7efc623752bb10178f77c57d57bc07f72738ff82
haoranyu <yu.haora@northeastern.edu>
2020-12-16 04:06:23 -0500

    DOM-manipulation

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,19 @@
-'use strict';
+'use strict'
+
+// DOM manipulation
+
+// read the content of the element
+console.log(document.querySelector('.message').textContent);
+
+// change the content of the element
+document.querySelector('.message').textContent = 'Correct Number'
+
+// log new content
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
